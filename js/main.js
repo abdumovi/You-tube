6a047a82c9e613569bc2f4360609253812e31a79
abdurahmon <abdumovi@gmail.com>
2021-11-29 16:28:25 +0500

    end

--- js/main.js
+++ js/main.js
@@ -13,7 +13,55 @@ let items = document.querySelector('.hero__row');
 let filterName = document.querySelectorAll('.hero__iframe-title');
 let userBtn = document.querySelector('.header--user');
 let userbar = document.querySelector('.user-bar');
+// ==========================modals
+let bel = document.querySelector('.bell');
+let belBtn = document.querySelector('.bell-btn');
+let frame = document.querySelector('.frame');
+let frameBtn = document.querySelector('.frame-btn');
+let addVideo = document.querySelector('.add-video__block');
+let addVideoBtn = document.querySelector('.add-video');
+let modal = document.querySelector('.modal');
+let modalBtn = document.querySelector('.voise--btn');
+let modalClose = document.querySelector('.modal__btn');
 
+modalClose.addEventListener('click',function(){
+  modal.classList.remove('db-block');
+  darcSpan.classList.remove('span--block');
+  bod.classList.remove('over');
+});
+
+modalBtn.addEventListener('click',function(){
+  modal.classList.toggle('db-block');
+  darcSpan.classList.add('span--block');
+  bod.classList.add('over');
+});
+
+addVideoBtn.addEventListener('click',function(){
+  addVideo.classList.toggle('db-block');
+  frame.classList.remove('db-block');
+});
+
+addVideoBtn.addEventListener('blur',function(){
+  addVideo.classList.remove('db-block');
+  
+});
+
+
+frameBtn.addEventListener('click',function(){
+  frame.classList.toggle('db-block');
+  addVideo.classList.remove('db-block');
+});
+frameBtn.addEventListener('blur',function(){
+  frame.classList.remove('db-block');
+});
+belBtn.addEventListener('click',function(){
+  bel.classList.toggle('db-block');
+  frame.classList.remove('db-block');
+});
+
+belBtn.addEventListener('blur',function(){
+  bel.classList.remove('db-block');
+});
 //===============================span-darc
 
 darcSpan.addEventListener('click',function(){
@@ -21,6 +69,7 @@ darcSpan.addEventListener('click',function(){
   darcSpan.classList.remove('span--block');
   bod.classList.remove('over');
   userbar.classList.remove('user-bar-block');
+  modal.classList.remove('db-block');
 });
 
 //=========================nav-left
@@ -54,9 +103,10 @@ elnavBtn.addEventListener('click',function(){
 userBtn.addEventListener('click',function(){
   userbar.classList.toggle('user-bar-block');
   elNavSecond.classList.remove('hero__second-nav-tog');
-  darcSpan.classList.toggle('span--block');
 });
-
+userBtn.addEventListener('blur',function(){
+  userbar.classList.remove('user-bar-block');
+});
 //====================================darc
 
 darcbtn.addEventListener('click',function(){
